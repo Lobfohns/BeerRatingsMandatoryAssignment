1a6215fe537c2159f21ffd607dc8429f6f891440
Lobfohns <ludv1428@stud.kea.dk>
2021-10-16 16:14:03 +0200

    tester jenkins

--- src/main/java/com/example/beertasting/controllers/Beers.java
+++ src/main/java/com/example/beertasting/controllers/Beers.java
@@ -15,7 +15,7 @@ public class Beers {
 
     @GetMapping("/beers")
     public Iterable<Beer> getBeers(){
-        System.out.println("hej jon");
+        System.out.println("tester noget hej");
         return beers.findAll();
     }
 
